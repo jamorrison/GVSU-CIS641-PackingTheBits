79d2c4438a5a99e2a70e5f80c3b11f64c1ec34b3
Jacob Morrison <Jacob.Morrison@vai.org>
2024-10-17 13:29:16 -0400

    modify testing file

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,4 @@
 BIN=../src/build/src
 
-#${BIN}/coverage -@ 1 -s 1000000 new.bam > tempas1
-${BIN}/coverage -@ 1 -s 100 new.bam > tempas1
+${BIN}/coverage -@ 1 -s 1000000 new.bam > tempas1
+valgrind --tool=memcheck --leak-check=full ${BIN}/coverage -@ 1 -s 1000000 new.bam > tempas1
