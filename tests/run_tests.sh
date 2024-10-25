6bf67759d438643004680dda3f33776d1d8ac0ad
Jacob Morrison <Jacob.Morrison@vai.org>
2024-10-25 15:21:27 -0400

    add multithreaded test run

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,3 +6,4 @@ BOT=windows100bp.gc_content.bot10p.bed.gz
 valgrind --tool=memcheck --leak-check=full ${BIN}/coverage -b ${BOT} -t ${TOP} -p new -@ 1 -s 1000000 ${CPG} new.bam
 ${BIN}/coverage -b ${BOT} -t ${TOP} -p new -@ 1 -s 1000000 ${CPG} new.bam
 
+${BIN}/coverage -b ${BOT} -t ${TOP} -p new -@ 8 -s 1000000 ${CPG} new.bam
